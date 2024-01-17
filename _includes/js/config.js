39f0c59fd73454d038e9e39c82b9b3765aa6a078
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-01-17 12:57:42 +0100

    init

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,37 +1,78 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary
-}});
-
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary,
-  fontSize: '16px'
-}});
+var siteTheme = gbifReactComponents.themeBuilder.extend({
+  baseTheme: 'light', extendWith: {
+    primary: themeStyle.colors.primary,
+    linkColor: themeStyle.colors.links,
+    fontSize: '16px'
+  }
+});
 
 var siteConfig = {
+  version: 2,
   routes: {
+    alwaysUseHrefs: false, // Update - there now is translations. since the site isn't translated we can use push for now. if true, then we will always use hrefs, if false we will use onClick events and push state to the history. I've added this because I just realize that the language picker doesn't work with pushState as the url of the translated site is not updated with the new url
+    enabledRoutes: ['occurrenceSearch', 'collectionSearch', 'collectionKey', 'institutionKey'],
     occurrenceSearch: {
-      // The route you are currently using for occurrence search. The language prefix will be added automatically
-      // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
-      // route: '/data'
+      route: '/specimen/search'
     }
   },
   occurrence: {
+    excludedFilters: ['occurrenceStatus', 'networkKey', 'institutionKey', 'hostingOrganizationKey', 'protocol', 'publishingCountryCode', 'institutionCode', 'collectionCode'],
+    highlightedFilters: ['taxonKey', 'verbatimScientificName', 'collectionKey', 'catalogNumber', 'recordedBy', 'identifiedBy'],
+    defaultTableColumns: ['features', 'collectionKey', 'catalogNumber', 'country', 'year', 'recordedBy', 'identifiedBy'],
+    availableCatalogues: ['INSTITUTION', 'COLLECTION', 'OCCURRENCE'],
     mapSettings: {
-      lat: 60,
-      lng: -100,
-      zoom: 4.9115440763665068
+      lat: 0,
+      lng: 0,
+      zoom: 0
     },
     // You probably need help to configure the scope - so just ask
     // for his demo site we only show Fungi (taxonKey=5). It use the predicate structure known from GBIF download API. 
     // See https://www.gbif.org/developer/occurrence (long page without enough anchors - search for "Occurrence Download Predicates")
     // The format is however slightly different, in that is use camelCase for keys instead of CONSTANT_CASE. 
-    rootPredicate: { type: 'equals', key: 'taxonKey', value: 5 }, 
-    // occurrenceSearchTabs: ['MAP', 'TABLE', 'GALLERY', 'DATASETS'] // what tabs should be shown
+    rootPredicate: {
+      "type": "and",
+      "predicates": [
+        {
+          "type": "or",
+          "predicates": [
+            {
+              "type": "isNotNull",
+              "key": "institutionKey"
+            },
+            {
+              "type": "isNotNull",
+              "key": "collectionKey"
+            }
+          ]
+        },
+        {
+          "type": "equals",
+          "key": "publishingOrg",
+          "value": "eac88d99-9f6c-4031-8fc4-8088f0e0dfe7"
+        }
+      ]
+    },
+    occurrenceSearchTabs: ['TABLE', 'MAP'] // what tabs should be shown
     // see https://hp-theme.gbif-staging.org/data-exploration-config for more options
+  },
+  collection: {
+    rootFilter: {
+      displayOnNHCPortal: true
+    }
+  },
+  availableCatalogues: ['OCCURRENCE', 'COLLECTION'],
+  maps: {
+    // locale: 'ja',
+    defaultProjection: 'MERCATOR',
+    defaultMapStyle: 'BRIGHT',
+    mapStyles: {
+      ARCTIC: ['NATURAL', 'BRIGHT'],
+      PLATE_CAREE: ['NATURAL', 'BRIGHT', 'DARK'],
+      MERCATOR: ['NATURAL', 'BRIGHT', 'SATELLITE', 'DARK'],
+      ANTARCTIC: ['NATURAL', 'BRIGHT', 'DARK']
+    }
+  },
+  messages: {
+    "catalogues.occurrences": "Especímenes"
   }
 };
-
-// example of a language specific route overwrite
-// if (pageLang === 'da')  {
-//   siteConfig.routes.occurrenceSearch.route = '/observationer/sog';
-// }
--- home.md
+++ home.md
@@ -1,14 +1,14 @@
 ---
 lang-ref: home
 layout: home
-title: Template headline
+title: Colecciones Científicas
 description: Lesser saw seas own have can’t kind Also to every own yielding there stars one itself lights seed yielding dominion lesser from lesser were divide be their spirit one behold a they’re grass called open.
 background:  "{{ site.data.images.acalypha_gillespieae.src }}"
 imageLicense: "{{ site.data.images.acalypha_gillespieae.caption }}"
 height: 90vh
 cta:
-  - text: Institutions
-    href: /institution/search
+  - text: Collections
+    href: /collection/search
     isPrimary: true
   - text: Specimens
     href: /specimen/search
